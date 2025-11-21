542048318f2127a52d52da6c08b26f34f48bfcb3
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-11-21 15:04:13 +0530

    UC1: Added AddressBookService with AddContact method

---
+++ AddressBook.Application/Services/AddressBookService.cs
@@ -0,0 +1,20 @@
+﻿using AddressBook.Core.Models;
+using System;
+ 
+namespace AddressBook.Application.Services
+{
+    public class AddressBookService
+    {
+        private readonly List<Contact> _contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public IEnumerable<Contact> GetAllContacts()
+        {
+            return _contacts;
+        }
+    }
+}
--- AddressBook.Core/Models/Contact.cs
+++ AddressBook.Core/Models/Contact.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AddressBook.Core.Models
 {
-    internal class Contact
+    public class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email {  get; set; }
     }
 }
